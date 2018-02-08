b764160fac3c291987f777d7b64cf4e44b3368a9
enirobot <enirobot@naver.com>
2018-02-08 11:42:15 +0900

    이전에 만든 프로젝트 따로 분리

---
+++ PillingTime(old)/PillingTime/HomeImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeImageCollectionViewCell.swift
+//  PillingTime
+//
+//  Created by Enirobot on 2018. 2. 3..
+//  Copyright © 2018년 Enirobot. All rights reserved.
+//
+
+import UIKit
+
+class HomeImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collectionImageView: UIImageView!
+}
