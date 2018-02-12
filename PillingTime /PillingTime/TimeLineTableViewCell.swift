e83c9be1c9e6700492c1ff1261dde6bc5dd537c4
yurishin <dmine199@gmail.com>
2018-02-12 14:14:45 +0900

    #17
    새로운 UI이에 대응하여 PageController를 이용한 홈화면과 Setting page 및 Add page 추가

---
+++ PillingTime /PillingTime/TimeLineTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TimeLineTableViewCell.swift
+//  PillingTime
+//
+//  Created by Enirobot on 2018. 2. 8..
+//  Copyright © 2018년 Enirobot. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var memoLabel: UILabel!
+    @IBOutlet weak var morningButton: UIButton!
+    @IBOutlet weak var lunchButton: UIButton!
+    @IBOutlet weak var dinnerButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
